35ab453b078f5238ab3150e3c27436a0e93634b5
Angeliki <angiesintichaki@outlook.com>
2024-05-05 12:21:38 +0300

    Correcting Identation in check_guess function

--- milestone_3.py
+++ milestone_3.py
@@ -6,8 +6,15 @@ word = random.choice(word_list)
 
 print(word)
 
-def check_guess(guess):#Task2/Step1
-    guess = guess.lower()# Task2/Step2
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
 while True:
     guess = input("Enter a single character: ")
 
@@ -16,11 +23,7 @@ while True:
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:#step /task 2
-    print(f"Good guess! {guess} is in the word.")
 
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
 
 
     

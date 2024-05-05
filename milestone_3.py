e1a1c7261a07abfcec9dab73f19f13a68cddaf94
Angeliki <angiesintichaki@outlook.com>
2024-05-05 11:13:29 +0300

    Task 2_Steps 1,2,3

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,12 @@
+import random
+
+word_list = ["apple", "banana","cherry","kiwi", "orange"]
+
+word = random.choice(word_list)
+
+print(word)
+
+
 while True:
     guess = input("Enter a single character: ")
 
@@ -5,3 +14,12 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:#step /task 2
+    print(f"Good guess! {guess} is in the word.")
+
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+    

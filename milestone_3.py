1d75b51b24505b270395e55ae52c4495e04171d3
Angeliki <angiesintichaki@outlook.com>
2024-05-05 10:57:40 +0300

    Merge branch 'main' of https://github.com/Angeliki1983/Hangman_2

--- milestone_3.py
+++ milestone_3.py
@@ -1,2 +1,7 @@
 while True:
-    
+    guess = input("Enter a single character: ")
+
+    if guess == 1 and guess.isalpha():
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")

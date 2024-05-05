8da1b4611d1d4b28bf62c0869d757fa7b88a3c49
Angeliki <angiesintichaki@outlook.com>
2024-05-05 18:00:19 +0300

    define ask_for_input function milestone_3/Task 3

--- milestone_3.py
+++ milestone_3.py
@@ -15,14 +15,16 @@ def check_guess(guess):
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
-while True:
-    guess = input("Enter a single character: ")
+def ask_for_input():
+    while True:
+        guess = input("Enter a single character: ")
 
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
 
+check_guess(guess)
 
 
 

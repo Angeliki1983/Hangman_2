e84ac0470b3a38ccdbc3162f6f0f7c52f1e697ec
Angeliki <angiesintichaki@outlook.com>
2024-05-07 12:26:14 +0300

    milestone_4/Task1/Step 1, 2 ,3

--- milestone_4.py
+++ milestone_4.py
@@ -6,24 +6,29 @@ word = random.choice(word_list)
 
 print(word)
 
-def check_guess(guess):
-    guess = guess.lower()
+# def check_guess(guess):
+#     guess = guess.lower()
 
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
+#     if guess in word:
+#         print(f"Good guess! {guess} is in the word.")
 
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
+#     else:
+#         print(f"Sorry, {guess} is not in the word. Try again.")
 
-def ask_for_input():
-    while True:
-        guess = input("Enter a single character: ")
+# def ask_for_input():
+#     while True:
+#         guess = input("Enter a single character: ")
 
-        if len(guess) == 1 and guess.isalpha():
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+#         if len(guess) == 1 and guess.isalpha():
+#             break
+#         else:
+#             print("Invalid letter. Please, enter a single alphabetical character.")
 
-    check_guess(guess) #Milestone 3/step 3
+#     check_guess(guess) #Milestone 3/step 3
 
-ask_for_input()
+# ask_for_input()
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):#Mileston4/Task1/Step1,2,3
+        self.word = random.choice(word_list) #1
+        self.word_guessed = 

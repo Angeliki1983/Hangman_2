5ea4642d0eef2930d33f49554885e9a545edc701
Angeliki <angiesintichaki@outlook.com>
2024-05-08 18:40:50 +0300

    Code from milestone 3 deleted

--- milestone_4.py
+++ milestone_4.py
@@ -1,27 +1,4 @@
 import random
-
-# def check_guess(guess):
-#     guess = guess.lower()
-
-#     if guess in word:
-#         print(f"Good guess! {guess} is in the word.")
-
-#     else:
-#         print(f"Sorry, {guess} is not in the word. Try again.")
-
-# def ask_for_input():
-#     while True:
-#         guess = input("Enter a single character: ")
-
-#         if len(guess) == 1 and guess.isalpha():
-#             break
-#         else:
-#             print("Invalid letter. Please, enter a single alphabetical character.")
-
-#     check_guess(guess) #Milestone 3/step 3
-
-# ask_for_input()
-
 class Hangman:
     def __init__(self, word_list, num_lives=5):#Mileston4/Task1/Step1,2,3
         self.word = random.choice(word_list) #1
@@ -50,14 +27,12 @@ class Hangman:
                 self.check_guess(guess)
 
 #test game
+word_list = ["apple", "banana","cherry","kiwi", "orange"]
+
 Hangman_game = Hangman("apple")
 Hangman_game.ask_for_input() #calling ask_for_input_function to start the game
 
     
 
 
-word_list = ["apple", "banana","cherry","kiwi", "orange"]
 
-word = random.choice(word_list)
-
-print(word)
